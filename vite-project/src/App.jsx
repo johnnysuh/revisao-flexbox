43793950fc9b7d650941291565376bd28c78d98a
johnnysuh <nicolas.c.duarte@estudante.sesisenai.org.br>
2024-05-06 09:30:17 -0300

    flexbox

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Flexbox from './components/Flexbox'
+
+function App() {
+ 
+  return (
+    <>
+     <Flexbox/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Flexbox.jsx
@@ -0,0 +1,15 @@
+export default function Flexbox() {
+    return (
+        <section className="flexbox">
+            <div className="item1">
+                <p>Item 1</p>
+            </div>
+            <div className="item2">
+                <p>Item 2</p>
+            </div>
+            <div className="item3">
+                <p>Item 3</p>
+            </div>
+        </section>
+    )
+    }
